45f42f4edd38977f5d3bc330d964b653daa69fc6
diyi0617 <u11002265@o365.ttu.edu.tw>
2024-05-06 22:06:41 +0800

    修改1.2題
    利用副程式完成作業，並將變數改為i 、j、 z，密碼的變數改為pass。

--- HW2.c
+++ HW2.c
@@ -1,12 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
-#include<string.h>
+#include<math.h>
 
-int main(void)
+int i,j,z;
+char a;
+
+int show_1(void);
+int show_2(void);
+int show_3(void);
+int show_4(void);
+
+
+int main()
 {
-	int i,j,z,n;
+	int pass;
+	i=0;
 	
+	show_1();//顯示個人畫面 
+	system("pause");
+	system("cls"); 
+	
+	while(1)//輸入密碼 
+	{
+		printf("請輸入密碼:");
+		scanf("%d",&pass);
+		if(pass==2024)//密碼正確 
+			break;
+		else//密碼錯誤 
+			i++;
+		
+		if(i==3)//密碼錯誤3次 
+		{
+			printf("錯誤!\a");
+			system("pause");
+			return 0;
+		}
+	}
+	
+	while(1)//執行程式
+	{
+		system("cls");
+		fflush(stdin);
+		show_2();
+		a=getch();
+		system("cls");
+		switch(a)
+		{
+			case'a':
+			case'A':
+				show_3();
+				break;
+				
+			case'b':
+			case'B':
+				show_4();
+				break;
+				
+			case'c':
+			case'C':
+				while(1)
+				{
+					printf("Continue?(y/n)");
+					a=getch();
+					if(a=='y'||a=='Y')
+						break;
+					else if(a=='n'||a=='N')
+						return 0;
+					else
+						printf("  error\n");
+				}		
+		}
+		
+	} 
+}
+
+show_1()
+{
 	printf("---------------------------------------------\n");
 	printf("EEEEEEEEEE      333333333       BBBBBBBB     \n");
 	printf("E                       3       B       B    \n");
@@ -28,34 +98,11 @@ int main(void)
     printf("       │●　　　　│                        \n");
     printf("       │　　　　　╰──╮                  \n");
     printf("       ╰────────╯                  \n");
-    printf("---------------------------------------------\n");//顯示個人畫面
-    system("pause");
-	system("CLS");
-	
-	char in;
-	
-	i=0;
-	while(1)//輸入密碼 
-	{
-		printf("請輸入密碼:\n");
-		scanf("%d",&n);//輸入密碼 
-		i++;
-		
-		if(n==2024)//密碼正確 
-			break;//跳出while迴圈 
-		else
-			printf("錯誤!\n");
-		
-		if(i==3)//錯誤累積三次 
-		{
-			printf("錯誤三次!\a");
-			system("pause");
-			return 0;//結束程式 
-		}	
-			
-	}
-	system("CLS");
-	
+    printf("---------------------------------------------\n");
+}
+
+show_2()
+{
 	printf("-----------------------------\n");
 	printf("|    a. 畫出直角三角形      |\n");
 	printf("|    b. 顯示乘法表          |\n");
@@ -63,3 +110,5 @@ int main(void)
 	printf("-----------------------------\n");//主選單
 	printf("請輸入 :");
 }
+
+
